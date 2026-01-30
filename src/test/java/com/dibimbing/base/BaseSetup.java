db5714004f7e3b3b725df39fce8a328f001e2c2f
IkhsanPramandika <mikhsanpra26@gmail.com>
2026-01-31 02:56:30 +0700

    update browser

--- src/test/java/com/dibimbing/base/BaseSetup.java
+++ src/test/java/com/dibimbing/base/BaseSetup.java
@@ -1,6 +1,5 @@
 package com.dibimbing.base;
 
-import com.dibimbing.pages.login.LoginPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,50 +12,49 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
-
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
 import java.time.Duration;
+import java.util.Properties;
 
 public class BaseSetup {
-    public WebDriver driver;
-    public Properties properties;
-    public WebDriverWait wait;
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected Properties properties;
 
     @BeforeMethod
     @Parameters("browser")
     public void setup(@Optional("chrome") String browser) {
+        try {
+            properties = new Properties();
+            FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
+            properties.load(fis);
+        } catch (IOException e) {
+            throw new RuntimeException("Gagal memuat file config.properties: " + e.getMessage());
+        }
 
         if (browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
             ChromeOptions options = new ChromeOptions();
+
             options.addArguments("--headless=new");
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--window-size=1920,1080");
+            options.addArguments("--remote-allow-origins=*");
+
             driver = new ChromeDriver(options);
-        }
-        // Konfigurasi Firefox
-        else if (browser.equalsIgnoreCase("firefox")) {
+        } else if (browser.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
             FirefoxOptions options = new FirefoxOptions();
             options.addArguments("--headless");
             driver = new FirefoxDriver(options);
         }
 
+
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         driver.get(properties.getProperty("url"));
-    }
-
-    @BeforeMethod
-    public void setupLogin() {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.login(
-                properties.getProperty("admin_email"),
-                properties.getProperty("admin_password")
-        );
     }
 
     @AfterMethod
