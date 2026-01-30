89aecedf4101403d8316ea37953fad2b889ce16e
IkhsanPramandika <mikhsanpra26@gmail.com>
2026-01-31 02:35:03 +0700

    update detik browser

--- src/test/java/com/dibimbing/base/BasePage.java
+++ src/test/java/com/dibimbing/base/BasePage.java
@@ -14,7 +14,7 @@ public class BasePage {
     public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     }
 
     protected void waitForElementToBeVisible(WebElement element) {
