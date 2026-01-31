4f664546de02abeddb9467bfd93fab6b7e0dce78
IkhsanPramandika <mikhsanpra26@gmail.com>
2026-02-01 01:31:57 +0700

    update excel

--- src/test/java/com/dibimbing/tests/employee/EmployeeTest.java
+++ src/test/java/com/dibimbing/tests/employee/EmployeeTest.java
@@ -1,6 +1,7 @@
 package com.dibimbing.tests.employee;
 
 import com.dibimbing.base.BaseLoginUI;
+import com.dibimbing.base.BaseSetup;
 import com.dibimbing.pages.employee.EmployeePage;
 import com.dibimbing.utils.CSVReader;
 import org.testng.Assert;
@@ -9,23 +10,20 @@ import org.testng.annotations.Test;
 
 public class EmployeeTest extends BaseLoginUI {
 
-
     @DataProvider(name = "employeeCSVData")
     public Object[][] getEmployeeCSVData() {
         return CSVReader.readCSV("src/test/resources/testdata/employee_data.csv");
     }
 
-
     @Test(dataProvider = "employeeCSVData")
-    public void testAddEmployeeFromCSV(String name, String empId, String email, String phone, String division, String role, String address) {
+    public void testAddEmployeeFromCSV(String name, String id, String email, String phone, String div, String role, String addr) {
         EmployeePage employeePage = new EmployeePage(driver);
-
         employeePage.goToEmployeePage();
         employeePage.clickAddEmployee();
 
-        employeePage.addEmployee(name, empId, email, phone, division, role, address);
+        employeePage.fillEmployeeData(name, id, email, phone, div, role, addr);
         employeePage.submitForm();
 
-        Assert.assertTrue(driver.getCurrentUrl().contains("employee"), "Gagal menambahkan karyawan dengan ID: " + empId);
+        Assert.assertTrue(driver.getCurrentUrl().contains("employee"), "Gagal menambahkan karyawan!");
     }
 }
